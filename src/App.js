2ba74df4c7b359c4b6af1e0d7a04c6937e67b4b9
NaanArmaX <tainan56.tss@gmail.com>
2024-02-21 18:11:31 -0300

    projeto imc pronto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState} from 'react';
+import './app.css';
+
+
+export default function App(){
+
+  const [peso,setPeso] = useState('');
+  const [altura,setAltura] = useState('');
+  
+  const[mensagem, setMensagem] = useState('');
+
+
+  function calcIMC(){
+    const alt= altura / 100;
+    const imc = peso / (alt * alt);
+
+     if(imc < 18.6){
+      setMensagem("Você está abaixo do peso! seu IMC: " + imc.toFixed(2))
+
+     }else if(imc >= 18.6 && imc < 24.9){
+      setMensagem("Peso Ideal! Seu IMC: " + imc.toFixed(2))
+     }else if (imc > 24.9){
+      setMensagem("O Programador Ficou Com Preguiça de fazer o resto :) Seu IMC: " + imc.toFixed(2))
+     }
+  }
+
+  return(
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Quer Calcular Seu IMC ?</span>
+
+      <div className="area-input">
+
+      <input  type="text" placeholder="Peso em (kg) Ex 90" value={peso} onChange={(e)=>setPeso(e.target.value)}/>
+      <input  type="text" placeholder="Altura em (cm) Ex 180" value={altura} onChange={(e)=>setAltura(e.target.value)} />
+
+      <button onClick={calcIMC}>Calcular</button>
+      </div>
+
+      <h2>{mensagem}</h2>
+
     </div>
-  );
-}
 
-export default App;
+    
+
+  )
+}
